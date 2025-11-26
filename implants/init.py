8317d89e7f79025e71e986eca9d304025a5f9336
ridpath <mike.ridpath@gmail.com>
2025-11-25 16:16:21 -0800

    feat: update implants/__init__.py to export MacOSImplant for cross-platform support

---
+++ implants/init.py
@@ -0,0 +1,6 @@
+from .base_implant import BaseImplant
+from .linux_implant import LinuxImplant
+from .windows_implant import WindowsImplant
+from .macos_implant import MacOSImplant
+
+__all__ = ['BaseImplant', 'LinuxImplant', 'WindowsImplant', 'MacOSImplant']
